43ed3a434f1f0bac0f060bf0fff449ef590f5348
Eugene Van Zyl <eugevanz@gmail.com>
2022-05-27 06:33:23 +0200

    project started

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-  $("p").click(function(){
-    $(this).hide();
+$(function () {
+  let reviews = $('.reviews');
+
+  let newLine = $('<li></li>').css({
+    display: 'flex',
+    'flex-wrap': 'nowrap',
+    alignItems: 'center'
   });
+  newLine.append(
+    $('<div></div>').text(`Line ${reviews.length}`).css({ minWidth: '10vw' })
+  );
+  newLine.append(
+    $('<input></input>')
+      .prop('disabled', true)
+      .css({ marginRight: '2vw', width: '100%' })
+  );
+  newLine.append($('<button>X</button>'));
+
+  reviews.append(newLine);
 });
