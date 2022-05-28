f286e5c92b0c506d97ce36c6a9d3d32a2b54ee1f
Eugene Van Zyl <eugevanz@gmail.com>
2022-05-28 13:44:45 +0200

    Fixing sync

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 $(function () {
   let reviews = $('.reviews');
+  let message = '';
 
-  let newLine = $('<li></li>').css({
-    display: 'flex',
-    'flex-wrap': 'nowrap',
-    alignItems: 'center'
-  });
-  newLine.append(
-    $('<div></div>').text(`Line ${reviews.length}`).css({ minWidth: '10vw' })
-  );
-  newLine.append(
-    $('<input></input>')
-      .prop('disabled', true)
-      .css({ marginRight: '2vw', width: '100%' })
-  );
-  newLine.append($('<button>X</button>'));
+  function newLine() {
+    return $(
+      `<li class="new-line">
+      <div class="new-line-number">Line ${$('.new-line').length + 1}</div>
+      <input type="text" class="new-line-text"></input>
+      <button>X</button>
+    </li>`
+    );
+  }
 
   reviews.append(newLine);
+
+  $('.new-line-button').click(function () {
+    reviews.append(newLine);
+  });
+
+  $('.new-line-text')
+    .last()
+    .keyup(function (e) {
+      message = $(this).val();
+      console.log(message);
+      // if (e.keyCode !== 8) {
+      //   message = message + String.fromCharCode(e.keyCode);
+      // } else {
+      //   message = message.slice(0, -1);
+      // }
+
+      if (message.length > 0 && message.length % 50 === 0) {
+        $(this).prop('disabled', true);
+        reviews.append(newLine);
+        $('textarea').val(message);
+        $('.new-line-text').last().focus();
+        return;
+      }
+    });
 });
